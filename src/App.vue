5c6f455a91922ea2e686f1c4a0af7a8d953b081f
MengJiaXi <1040616435@qq.com>
2023-02-18 21:26:54 +0800

    perf: 优化app的ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <router-view />
+  <div class="game-app">
+    <router-view />
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
+<style lang="less" scoped>
+.game-app {
+  height: 100vh;
+  width: 100%;
+  background-color: #f3f5f9 !important;
+}
 
+@media screen and (min-width: 450px) {
+  .game-app {
+    width: 450px;
+    height: 100vh;
+  }
+}
 </style>
